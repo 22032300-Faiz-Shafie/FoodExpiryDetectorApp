d671b4d60dbe0598ab87d40b00a1365e1bd830f8
DonSiau <donsiau2004@gmail.com>
2024-05-02 06:18:37 +0800

    added screen navigation to add food screen

--- FoodExpireTracker/App.js
+++ FoodExpireTracker/App.js
@@ -1,30 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Pressable, Image, ScrollView, Button} from 'react-native';
-//this is just the logo for now. change later
+import { StyleSheet, Text, View, Pressable, Image, ScrollView, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AddFoodScreen from "./Screens/AddFoodScreen";
+
+const Stack = createNativeStackNavigator();
 const logoImg = require("./assets/favicon.png");
+const addImg = require("./assets/add.png")
 
 export default function App() {
   return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="addFoodScreen" component={AddFoodScreen} options={{ headerStyle: { backgroundColor: 'lightgreen' } }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
 
-
-      <View style={styles.backGround}>
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.backGround}>
       <ScrollView>
-      <View style={{flex:1,height:200,backgroundColor:"lightgreen", borderRadius:30}}>
-        <StatusBar backgroundColor="lightgreen" barStyle="default"/>
-        <Text style={{textAlign:"center", fontSize:20, paddingTop:20}}>Elsa's List</Text>
-        <Image source={logoImg} style={{width:100, height:100,alignSelf:"center"}}/>
-      </View>
+        <View style={{ flex: 1, height: 200, backgroundColor: "lightgreen", borderRadius: 30 }}>
+          <StatusBar backgroundColor="lightgreen" barStyle="default" />
+          <Text style={{ textAlign: "center", fontSize: 20, paddingTop: 40 }}>Elsa's List</Text>
+          <Image source={logoImg} style={{ width: 100, height: 100, alignSelf: "center" }} />
+        </View>
       </ScrollView>
-      </View>
-  
- 
+      <View style={styles.addFoodButton}>
+          <Pressable onPress={() => navigation.navigate("addFoodScreen")}>
+            <Image source={addImg} style={{ width: 70, height: 70 }} />
+          </Pressable>
+        </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  backGround: { 
-    flex: 1, 
-    justifyContent: "flex-start", 
-    backgroundColor: "#FFF5F5F5", 
+  backGround: {
+    flex: 1,
+    justifyContent: "flex-start",
+    backgroundColor: "#FFF5F5F5",
+  }, 
+  addFoodButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20
   }
 });
---
+++ FoodExpireTracker/Screens/AddFoodScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AddFoodScreen = () => {
+  return (
+    <View>
+      <Text>Add Food Screen</Text>
+    </View>
+  );
+}
+
+export default AddFoodScreen;
