ee61620f86fe906162fbe852d08974e8bf2fc5fd
DonSiau <donsiau2004@gmail.com>
2024-05-01 23:24:36 +0800

    created a layout for the UI

--- FoodExpireTracker/App.js
+++ FoodExpireTracker/App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Pressable, Image, ScrollView, Button} from 'react-native';
+//this is just the logo for now. change later
+const logoImg = require("./assets/favicon.png");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Food Expiry Tracker</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+      <View style={styles.backGround}>
+      <ScrollView>
+      <View style={{flex:1,height:200,backgroundColor:"lightgreen", borderRadius:30}}>
+        <StatusBar backgroundColor="lightgreen" barStyle="default"/>
+        <Text style={{textAlign:"center", fontSize:20, paddingTop:20}}>Elsa's List</Text>
+        <Image source={logoImg} style={{width:100, height:100,alignSelf:"center"}}/>
+      </View>
+      </ScrollView>
+      </View>
+  
+ 
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  backGround: { 
+    flex: 1, 
+    justifyContent: "flex-start", 
+    backgroundColor: "#FFF5F5F5", 
+  }
 });
