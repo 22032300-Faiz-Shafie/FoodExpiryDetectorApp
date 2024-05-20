de74c196b50ce454e3286e30e41e2fd616b8b859
Faiz <22032300@myrp.edu.sg>
2024-05-21 02:19:39 +0800

    Added a function, handleEditFruitAccuracyScore, to update the relevant Document Object with the new fields. Basically the insert and update of this function.

--- FoodExpireTracker/Screens/AiAccuracyForm.js
+++ FoodExpireTracker/Screens/AiAccuracyForm.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View, Pressable, Image, ScrollView, FlatList, SafeAre
 import DropDown from "react-native-paper-dropdown";
 import React, { useState, useEffect } from 'react';
 import { PaperProvider, Button, TextInput } from 'react-native-paper';
-import { doc, onSnapshot, query, collection, deleteDoc} from "firebase/firestore";
+import { doc, onSnapshot, query, collection, deleteDoc, addDoc, updateDoc} from "firebase/firestore";
 import {db} from "../firebaseConfig"
 import Slider from '@react-native-community/slider';
 
@@ -11,8 +11,9 @@ export default function App(){
     const [showDropDown, setShowDropDown] = useState(false);
     const [fruit, setFruit] = useState([]);
     const [selectedFruit, setSelectedFruit] = useState('');
-    const [remark, setRemark] = useState(""); 
+    const [aiAccuracyRemark, setAiAccuracyRemark] = useState(""); 
     const [sliderValue, setSliderValue] = useState(0);
+    const [dropdownKey, setDropdownKey] = useState(0);
     
     //Fetch Fruit Data to populate dropdown list -Faiz
     useEffect(() => {
@@ -30,6 +31,7 @@ export default function App(){
 
                 for(const fruit of fruitList){
                     fruitListDropDown.push({
+                        id: fruit.id,
                         label: fruit.data.foodName,
                         value: fruit.data.foodName,
                     })
@@ -43,7 +45,34 @@ export default function App(){
   
         fetchData();
       }, []);
+    
+    //Updates Document Object with the new fields that were selected before hitting submit -Faiz
+    const handleEditFruitAccuracyScore = async () =>{
+        try{
+            const fruitScoreData = {
+                aiAccuracyScore: sliderValue,
+                aiAccuracyRemark: aiAccuracyRemark
+            };
 
+            for(const fruitObject of fruit){
+                if(selectedFruit == fruitObject.value){
+                    const docRef = await updateDoc(doc(db, "foodCollection", fruitObject.id), fruitScoreData);
+                    console.log(`Document ${fruitObject.id} updated`);
+                }
+            }
+
+            setShowDropDown(false);
+            setSelectedFruit('');
+            setAiAccuracyRemark("");
+            setSliderValue(0);
+            setDropdownKey(prevKey => prevKey + 1);
+        }
+        catch(error){
+            console.error('Error updating document: ', error);
+        }
+
+
+    }
     
     //Still Deciding on how to implement Accuracy Score and using what metric. For now the Accuracy Form contains: Dropdown list including fruits from database, remark text input, and accuracy rating. -Faiz
     return(
@@ -51,14 +80,14 @@ export default function App(){
         <ScrollView style={{backgroundColor: "lightgreen"}}>
             <View>
                 <SafeAreaView style={styles.input2}>
-                <DropDown dropDownContainerHeight={150} label={'Fruit'} mode='flat' visible={showDropDown} showDropDown={() => setShowDropDown(true)} onDismiss={() => setShowDropDown(false)} value={selectedFruit} setValue={setSelectedFruit} list={fruit} dropDownStyle={styles.dropDownStyle}/>
+                <DropDown key={dropdownKey} dropDownContainerHeight={150} label={'Fruit'} mode='flat' visible={showDropDown} showDropDown={() => setShowDropDown(true)} onDismiss={() => setShowDropDown(false)} value={selectedFruit} setValue={setSelectedFruit} list={fruit} dropDownStyle={styles.dropDownStyle}/>
                 </SafeAreaView>
             </View>
             <View>
                 <TextInput 
                 style={styles.input2}
-                value={remark}
-                onChangeText={setRemark}
+                value={aiAccuracyRemark}
+                onChangeText={setAiAccuracyRemark}
                 label="Enter Remarks"
                 mode='flat'
                 />
@@ -68,7 +97,7 @@ export default function App(){
                 <Slider minimumValue={0} maximumValue={100} step={1.0} value={sliderValue} onValueChange={value => setSliderValue(value)} style={styles.input2} minimumTrackTintColor="green" maximumTrackTintColor="green" thumbTintColor="green"/>
             </View>
             <View style={styles.submitButton}>
-                <Button icon="upload" mode="contained-tonal" buttonColor="green">Submit</Button>
+                <Button icon="upload" mode="contained-tonal" buttonColor="green" onPress={handleEditFruitAccuracyScore}>Submit</Button>
             </View>
         </ScrollView>
         </PaperProvider>
@@ -86,6 +115,7 @@ const styles = StyleSheet.create({
     //This changes the position of the dropdown, for some reason by default the dropdown is in the middle of the screen -Faiz
     dropDownStyle: {
     marginTop: -50,
+    backgroundColor: "white"
     },
     text: {
         marginHorizontal: 12,
