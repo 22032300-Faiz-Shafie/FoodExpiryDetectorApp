a4ac18f7277bdd7cb40a18ce20e54be4b89972d3
Faiz <22032300@myrp.edu.sg>
2024-05-22 14:59:24 +0800

    Modified AiAccuracyForm, instead of updating the variables in the FoodCollection collection in the database, it instead puts all data in the new fruitAiAccuracyFormReports collection. Finished with insert function

--- FoodExpireTracker/Screens/AiAccuracyForm.js
+++ FoodExpireTracker/Screens/AiAccuracyForm.js
@@ -8,12 +8,17 @@ import Slider from '@react-native-community/slider';
 
 export default function App(){
     const foodsCol = collection(db, "foodCollection");
+    const fruitAiAccuracyFormReportsCol = collection(db, "fruitAiAccuracyFormReports");
     const [showDropDown, setShowDropDown] = useState(false);
     const [fruit, setFruit] = useState([]);
     const [selectedFruit, setSelectedFruit] = useState('');
     const [aiAccuracyRemark, setAiAccuracyRemark] = useState(""); 
     const [sliderValue, setSliderValue] = useState(0);
     const [dropdownKey, setDropdownKey] = useState(0);
+    const [aiAccuracyReport, setAiAccuracyReport] = useState([]);
+    const [aiAccuracyReportRefId, setAiAccuracyReportRefId] = useState("");
+    const [isAiAccuracyReportRefIdVisible, setIsAiAccuracyReportRefIdVisible] = useState(false);
+    const [newAiAccuracyReportRefId, setNewAiAccuracyReportRefId] = useState("");
     
     //Fetch Fruit Data to populate dropdown list -Faiz
     useEffect(() => {
@@ -46,6 +51,27 @@ export default function App(){
         fetchData();
       }, []);
     
+    //Fetch Fruit Ai Accuracy Reports Data -Faiz
+    useEffect(() => {
+        const q = query(fruitAiAccuracyFormReportsCol);
+        const fetchData = async () => {
+            const unsubscribe = onSnapshot(q, (querySnapshot) => {
+                const aiReportList  = [];
+                querySnapshot.forEach((doc) => {
+                    aiReportList.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                });
+
+                setAiAccuracyReport(aiReportList);
+            });
+            return () => unsubscribe();
+        }
+
+        fetchData();
+    }, [])
+
     //Updates Document Object with the new fields that were selected before hitting submit -Faiz
     const handleEditFruitAccuracyScore = async () =>{
         try{
@@ -73,11 +99,50 @@ export default function App(){
 
 
     }
+
+    const handleAddAiFruitAccuracyReport = async () =>{
+        try{
+
+            for(const fruitObject of fruit){
+                if(selectedFruit == fruitObject.value){{
+                    const fruitScoreData = {
+                        fruitReferenceId: fruitObject.id,
+                        aiAccuracyScore: sliderValue,
+                        aiAccuracyRemark: aiAccuracyRemark
+                    }
+
+                    const docRef = await addDoc(collection(db, 'fruitAiAccuracyFormReports'), fruitScoreData);
+                    console.log("The following Ai Accuracy Form Report Document was written with ID: ", docRef.id);
+                    setNewAiAccuracyReportRefId(docRef.id);
+                }}
+            }
+
+            setAiAccuracyReportRefId("");
+            setShowDropDown(false);
+            setSelectedFruit('');
+            setAiAccuracyRemark("");
+            setSliderValue(0);
+            setDropdownKey(prevKey => prevKey + 1);
+            setIsAiAccuracyReportRefIdVisible(true);
+        }
+        catch(error){
+            console.error('Error Adding Document: ', error);
+        }
+    }
     
     //Still Deciding on how to implement Accuracy Score and using what metric. For now the Accuracy Form contains: Dropdown list including fruits from database, remark text input, and accuracy rating. -Faiz
     return(
         <PaperProvider>
         <ScrollView style={{backgroundColor: "lightgreen"}}>
+            <View>
+                <TextInput 
+                style={styles.input2}
+                value={aiAccuracyReportRefId}
+                onChangeText={setAiAccuracyReportRefId}
+                label="Enter Reference ID (If Updating Or Deleting)"
+                mode='flat'
+                />
+            </View>
             <View>
                 <SafeAreaView style={styles.input2}>
                 <DropDown key={dropdownKey} dropDownContainerHeight={150} label={'Fruit'} mode='flat' visible={showDropDown} showDropDown={() => setShowDropDown(true)} onDismiss={() => setShowDropDown(false)} value={selectedFruit} setValue={setSelectedFruit} list={fruit} dropDownStyle={styles.dropDownStyle}/>
@@ -97,7 +162,20 @@ export default function App(){
                 <Slider minimumValue={0} maximumValue={100} step={1.0} value={sliderValue} onValueChange={value => setSliderValue(value)} style={styles.input2} minimumTrackTintColor="green" maximumTrackTintColor="green" thumbTintColor="green"/>
             </View>
             <View style={styles.submitButton}>
-                <Button icon="upload" mode="contained-tonal" buttonColor="green" onPress={handleEditFruitAccuracyScore}>Submit</Button>
+                <Button icon="upload" mode="contained-tonal" buttonColor="green" onPress={handleAddAiFruitAccuracyReport}>Submit</Button>
+            </View>
+            <View style={styles.updateButton}>
+                <Button icon="update" mode="contained-tonal" buttonColor="cyan" onPress={handleEditFruitAccuracyScore}>Update</Button>
+            </View>
+            <View style={styles.deleteButton}>
+                <Button icon="delete" mode="contained-tonal" buttonColor="red" onPress={handleEditFruitAccuracyScore}>Delete</Button>
+            </View>
+            <View>
+                {isAiAccuracyReportRefIdVisible ? (
+                    <View style={styles.input2}> 
+                        <Text>Thank you for submitting a Fruit Ai Accuracy Form Report. Here is the Reference ID: {newAiAccuracyReportRefId}</Text> 
+                    </View>
+                ): null}
             </View>
         </ScrollView>
         </PaperProvider>
@@ -124,7 +202,21 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         padding: 10,
         marginHorizontal: 100,
-        marginVertical: 10,
+        marginTop: 10,
+        width: '50%'
+    },
+    updateButton: {
+        borderRadius: 10,
+        padding: 10,
+        marginHorizontal: 100,
+        marginTop: 5,
+        width: '50%'
+    },
+    deleteButton: {
+        borderRadius: 10,
+        padding: 10,
+        marginHorizontal: 100,
+        marginTop: 5,
         width: '50%'
     },
 })
