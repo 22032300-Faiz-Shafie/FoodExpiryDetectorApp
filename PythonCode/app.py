7d7df18256019b6ea40008e45215563084ec9eac
DonSiau <donsiau2004@gmail.com>
2024-05-25 05:53:30 +0800

    commented out code on app.py

--- PythonCode/app.py
+++ PythonCode/app.py
@@ -4,6 +4,30 @@ from io import BytesIO
 import base64
 import uuid
 app = Flask(__name__)
+
+#This section probably not the best idea, probably should use JSON instead to put data into firestore
+#later edit in your own expiry days for your fruits -Don
+# fruit_expiry_days = {
+#     "Mango_Overripe": 1,
+#     "Mango_Ripe": 5,
+#     "Mango_Unripe": 8,
+#     "Pineapple_Overripe": 1,
+#     "Pineapple_Ripe": 1,
+#     "Pineapple_Unripe": 1,
+#     "Apple_Overripe": 1,
+#     "Apple_Ripe": 1,
+#     "Apple_Unripe": 1
+   
+# }
+# results ="9 Apple_Unripe"
+# def ExtractFruit_Class(results):
+#  for fruit_class in fruit_expiry_days:
+
+#      if fruit_class in results:
+#       splitFruit_Ripeness= str(fruit_class).split("_")
+#       data = {"foodName": splitFruit_Ripeness[0], "ripeness": splitFruit_Ripeness[1]}
+#       print(splitFruit_Ripeness[0]+splitFruit_Ripeness[1])
+    
 #adds image from camera to image folder, which is then used for computer vision -Don
 @app.route('/image', methods=['POST'])
 
@@ -14,17 +38,12 @@ def add_Image():
         decodedData = base64.b64decode((base64_image)) 
         image = Image.open(BytesIO(decodedData)) 
         image.save("images/newImage"+ str(uuid.uuid4())+".jpg", "JPEG") #the image here might not need to be stored, and instead just goes into computer vision directly -Don
-        return "success"
-
-
-        
+        return "success"   
     except Exception as e:
         print(e)
         return "failed"
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, Worlds!</p>"
+
 
 #use own IP address. Run Flask by pressing the "Run Python File" on top left  -Don
 if __name__ == "__main__":
